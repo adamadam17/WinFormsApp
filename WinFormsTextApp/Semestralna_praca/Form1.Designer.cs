07d21764539c8ed9303f72629a10c566c708b115
adam <95377516+adamadam17@users.noreply.github.com>
2025-03-06 11:01:17 +0100

    Add files via upload

---
+++ WinFormsTextApp/Semestralna_praca/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Semestralna_praca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(320, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(618, 426);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 88);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Zvoliť súbor";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(180, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Upraviť súbor";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "zmena veľkých písmen na malé",
+            "zmena malých písmen na veľké",
+            "zmeniť prvé písmeno vety na veľké",
+            "zmeniť prvé písmeno slova na veľké",
+            "odstránenie diakritiky"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 167);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(292, 106);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ WinFormsTextApp/Semestralna_praca/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Semestralna_praca
+{
+    public partial class Form1 : Form
+    {
+        private string filepath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            filepath = "";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.OK )
+            {
+                filepath = openFileDialog1.FileName;
+            }
+            else
+            {
+                filepath = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Clear();
+
+            if (filepath == "")
+            {
+                MessageBox.Show("Žiadny súbor nebol vybraný");
+                return;
+            }
+            try
+            {
+
+                string text = File.ReadAllText(filepath);
+                string originalText = text;
+                int pocet_zmien = 0;
+
+                if (checkedListBox1.CheckedItems.Contains("zmena veľkých písmen na malé") &&
+                     checkedListBox1.CheckedItems.Contains("zmena malých písmen na veľké"))
+                {
+                    MessageBox.Show("Nemôžete navzájom aplikovať 'Zmena veľkých písmen na malé' a 'Zmena malých písmen na veľké'.", "Konflikt transformácie", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+
+                    if (checkedListBox1.CheckedItems.Contains("zmena veľkých písmen na malé"))
+                    {
+                        text = text.ToLower();
+                        pocet_zmien += CountChanges(originalText, text);
+                    }
+
+                    if (checkedListBox1.CheckedItems.Contains("zmena malých písmen na veľké"))
+                    {
+                        text = text.ToUpper();
+                        pocet_zmien += CountChanges(originalText, text);
+                    }
+                }
+
+                if (checkedListBox1.CheckedItems.Contains("zmeniť prvé písmeno vety na veľké"))
+                {
+                    string changedText = Regex.Replace(text, @"(?:[.!?]\s*|^)(\w)", m => m.Value.ToUpper());
+                    pocet_zmien += CountChanges(originalText, changedText);
+                    text = changedText;
+                }
+
+                if (checkedListBox1.CheckedItems.Contains("zmeniť prvé písmeno slova na veľké"))
+                {
+                    string changedText = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.ToLower());
+                    pocet_zmien += CountChanges(originalText, changedText);
+                    text = changedText;
+                }
+
+                if (checkedListBox1.CheckedItems.Contains("odstránenie diakritiky"))
+                {
+                    string changedText = RemoveDiacritics(text);
+                    pocet_zmien += CountChanges(originalText, changedText);
+                    text = changedText;
+                }
+
+                SaveModifiedText(text, pocet_zmien);
+
+                richTextBox1.AppendText("Pôvodný text: \n" + originalText + "\n");
+                richTextBox1.AppendText("Upravený text: \n" + text + "\n");
+                richTextBox1.AppendText("Počet zmien: " + pocet_zmien + "\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba pri spracovaní súboru: " + ex.Message);
+            }
+        }
+
+        private int CountChanges(string originalText, string modifiedText)
+        {
+            int changes = 0;
+            for (int i = 0; i < originalText.Length; i++)
+            {
+                if (originalText[i] != modifiedText[i])
+                    changes++;
+            }
+            return changes;
+        }
+        private string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char znak in normalizedString)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(znak) != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(znak);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        private void SaveModifiedText(string text, int počet_zmien)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.AddExtension = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    sw.Write(text);
+                    sw.WriteLine("\nTotal changes: " + počet_zmien);
+                }
+                MessageBox.Show("File saved successfully at " + saveFileDialog.FileName);
+            }
+        }
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
